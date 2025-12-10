0d1f6d2cd375f3545c7208873008e1a519fab00c
Jorge Larach <jorgelarachesp@gmail.com>
2025-12-09 18:20:13 -0600

    completed initial wiring setup. uploaded picture

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -100,6 +100,10 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+	  HAL_Delay(100);
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+	  HAL_Delay(100);
   }
   /* USER CODE END 3 */
 }
