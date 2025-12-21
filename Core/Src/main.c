fc543e8bc267135bc1227277ff6e00bfc2170977
Jorge Larach <jorgelarachesp@gmail.com>
2025-12-21 01:21:41 -0600

    nearly finished arm design, included STLs and pictures, added ADC driver for servo debugging and assembly

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,7 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include <stdint.h>
+#include <stdio.h>
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -41,6 +41,8 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
 I2C_HandleTypeDef hi2c1;
 
 UART_HandleTypeDef huart2;
@@ -54,6 +56,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_I2C1_Init(void);
+static void MX_ADC1_Init(void);
 /* USER CODE BEGIN PFP */
 void PCA9685_set_bit(uint8_t reg, uint8_t bit, uint8_t val);
 void PCA9685_set_pwm_freq(uint16_t freq);
@@ -127,8 +130,7 @@ void PCA9685_init(uint16_t freq){
  * ON_L, ON_H, OFF_L, OFF_H
  * Masking and shifting on_time and off_time into respective locations in array
  * on_time will always be 0, so period always begins with high pulse
- * MODE1 bit 5 (AI) allows for contiguous memory writing
- * */
+ * MODE1 bit 5 (AI) allows for contiguous memory writing */
 void PCA9685_set_pwm(uint8_t servo, uint16_t on_time, uint16_t off_time){
 	uint8_t pwm[4];
 	uint8_t reg_address = PCA9685_PWM0_ON_L + (4 * servo);
@@ -181,9 +183,13 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   MX_I2C1_Init();
+  MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
   PCA9685_init(50); // Servos require 50hz frequency
 
+  uint32_t pot_val = 0;
+  uint8_t angle = 0;
+  char message[40] = {'\0'};
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -193,12 +199,33 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
-	  PCA9685_set_servo_angle(1, 0);
-	  HAL_Delay(1000);
+	  HAL_ADC_Start(&hadc1);
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET); // To visually see update frequency
+
+	  if(HAL_ADC_PollForConversion(&hadc1, 10) == HAL_OK){
+		  pot_val = HAL_ADC_GetValue(&hadc1); // Polling for potentiometer ADC (testing/assembly purposes)
+	  }
+
+	  angle = (pot_val * 180) / 4095; // Map pot value (12 bits) to angle
+	  if (angle > 180) angle = 180;
+
+	  // Transmitting to UART for debugging
+	  sprintf(message,
+	          "Potentiometer Val: %lu Angle: %u\r\n",
+			  pot_val,
+	          angle);
+
+	  HAL_UART_Transmit(&huart2,
+	                    (uint8_t*)message,
+	                    strlen(message),
+	                    100);
+
 	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
-	  PCA9685_set_servo_angle(1, 180);
-	  HAL_Delay(1000);
+
+	  PCA9685_set_servo_angle(1, angle);
+	  HAL_ADC_Stop(&hadc1); // Don't need this here
+	  HAL_Delay(100);
+
   }
   /* USER CODE END 3 */
 }
@@ -247,6 +274,58 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = 1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
 }
 
 /**
